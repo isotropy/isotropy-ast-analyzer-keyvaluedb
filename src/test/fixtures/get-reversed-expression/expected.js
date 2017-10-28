684cc962394518fd8f8e99efcf0908bcfd9883b1
Jeswin Kumar <jeswinpk@agilehead.com>
2017-10-28 18:55:04 +0530

    Remove unused test

--- src/test/fixtures/get-reversed-expression/expected.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  identifier: "myDb",
-  database: { connection: "redis://127.0.0.1:6379" },
-  operation: "get",
-  key: {
-    type: "StringLiteral",
-    value: "Task"
-  }
-};
--- src/test/fixtures/get-reversed-expression/fixture.js
+++
@@ -1,5 +0,0 @@
-import myDb from "../my-db";
-
-async function getTodos(who) {
-  return myDb.todos.find(todo => "Task" === todo.key);
-}
